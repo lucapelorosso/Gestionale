10ec5a16cc6980ab5916ebf8359a8c82bd2c9d61
Luca Pelorosso <lpelo2000@gmail.com>
2022-10-16 18:24:36 +0200

    estensione file database

---
+++ ScriptSql/DDL/ddl_create_database.sql
@@ -0,0 +1,7 @@
+/************************************************/
+/* Created by: Luca Pelorosso                   */
+/* Created: 2022-10-16                          */
+/* Modified by: Luca Pelorosso                  */
+/* Modified: 2022-10-16                         */
+/************************************************/
+CREATE DATABASE gestionale;
