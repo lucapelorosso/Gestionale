45674c65302eaf2f06ff68fe5607ac5fa116e7f8
Luca Pelorosso <lpelo2000@gmail.com>
2022-10-19 05:59:08 +0200

    definizione tabella account

--- ScriptSql/DDL/ddl_table_account.sql
+++ ScriptSql/DDL/ddl_table_account.sql
@@ -2,6 +2,15 @@
 /* Created by: Luca Pelorosso                   */
 /* Created: 2022-10-11                          */
 /* Modified by: Luca Pelorosso                  */
-/* Modified: 2022-10-16                         */
+/* Modified: 2022-10-19                         */
 /************************************************/
-create table gestionale.account;
+create table gestionale.account (
+    accountID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    birth_date date,
+    death_date date,
+    birth_city varchar(255),
+    death_city varchar(255),
+    addressID int
+);
