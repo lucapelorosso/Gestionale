2abdd77026d9c48a04db15cc1a52adba532813e6
Luca Pelorosso <lpelo2000@gmail.com>
2022-10-19 05:52:36 +0200

    rename

---
+++ ScriptSql/DDL/ddl_create-schema.sql
@@ -0,0 +1,7 @@
+/************************************************/
+/* Created by: Luca Pelorosso                   */
+/* Created: 2022-10-18                          */
+/* Modified by: Luca Pelorosso                  */
+/* Modified: 2022-10-18                         */
+/************************************************/
+CREATE SCHEMA `gestionale`;
