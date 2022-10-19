5d4eff087adab24c94f217a41614cc1a0d3690ce
Luca Pelorosso <lpelo2000@gmail.com>
2022-10-19 05:50:58 +0200

    drop user

--- ScriptSql/DML/dml:creazione_schema.sql
+++ ScriptSql/DML/dml:creazione_schema.sql
@@ -2,7 +2,12 @@
 /* Created by: Luca Pelorosso                   */
 /* Created: 2022-10-18                          */
 /* Modified by: Luca Pelorosso                  */
-/* Modified: 2022-10-18                         */
+/* Modified: 2022-10-19                         */
 /************************************************/
 
+-- Drop User
+DROP USER 'admin'@"ls-ffabf499b0f8332f4e19ea3361e9799960122e1b.cpbsrhnvchf7.eu-west-3.rds.amazonaws.com";
+
+
+-- Create User
 CREATE USER 'admin'@"ls-ffabf499b0f8332f4e19ea3361e9799960122e1b.cpbsrhnvchf7.eu-west-3.rds.amazonaws.com" IDENTIFIED BY 'gestionale';
