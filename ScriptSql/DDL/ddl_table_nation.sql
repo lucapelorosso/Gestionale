eb75a1520da81d4cb4259202a167fe930f7a13c4
Luca Pelorosso <lpelo2000@gmail.com>
2022-10-20 22:54:08 +0200

    new table

---
+++ ScriptSql/DDL/ddl_table_nation.sql
@@ -0,0 +1,10 @@
+/************************************************/
+/* Created by: Luca Pelorosso                   */
+/* Created: 2022-10-20                          */
+/* Modified by: Luca Pelorosso                  */
+/* Modified: 2022-10-20                         */
+/************************************************/
+create table gestionale.nation (
+    nationID int,
+    nation_name  varchar(255)
+);
