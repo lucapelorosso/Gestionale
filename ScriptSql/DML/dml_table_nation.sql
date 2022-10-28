faa24b332a0e7b7b90680b53bbadb916aafd110b
Luca Pelorosso <lpelo2000@gmail.com>
2022-10-28 18:20:59 +0200

    Insert tabella nazione

---
+++ ScriptSql/DML/dml_table_nation.sql
@@ -0,0 +1,7 @@
+/************************************************/
+/* Created by: Luca Pelorosso                   */
+/* Created: 2022-10-11                          */
+/* Modified by: Luca Pelorosso                  */
+/* Modified: 2022-10-16                         */
+/************************************************/
+insert gestionale.account;
