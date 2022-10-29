65f1d64d2cdc7e9162f107c227ab8463b27d640b
Luca Pelorosso <lpelo2000@gmail.com>
2022-10-29 14:52:09 +0200

    aggiunte nazioni

--- ScriptSql/DML/dml_table_nation.sql
+++ ScriptSql/DML/dml_table_nation.sql
@@ -1,7 +1,23 @@
 /************************************************/
 /* Created by: Luca Pelorosso                   */
-/* Created: 2022-10-11                          */
+/* Created: 2022-10-27                          */
 /* Modified by: Luca Pelorosso                  */
-/* Modified: 2022-10-16                         */
+/* Modified: 2022-10-28                         */
+/* Definition: insert data table nation         */
 /************************************************/
-insert gestionale.account;
+ 
+INSERT INTO gestionale.nation (nation_name)
+VALUES ('Italia'); 
+COMMIT;
+
+INSERT INTO gestionale.nation (nation_name)
+VALUES ('Francia'); 
+INSERT INTO gestionale.nation (nation_name)
+VALUES ('Germania'); 
+INSERT INTO gestionale.nation (nation_name)
+VALUES ('Spagna'); 
+INSERT INTO gestionale.nation (nation_name)
+VALUES ('Svizzera'); 
+
+
+COMMIT;
