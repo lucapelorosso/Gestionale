8305ee16251d906e5f685eb377d931d4ce7ba211
Luca Pelorosso <lpelo2000@gmail.com>
2023-02-23 19:49:45 +0100

    sript sh echo

--- ScriptUnix/Install.sh
+++ ScriptUnix/Install.sh
@@ -2,7 +2,7 @@
 /* Created by: Luca Pelorosso                   */
 /* Created: 2022-10-16                          */
 /* Modified by: Luca Pelorosso                  */
-/* Modified: 2022-10-16                         */
+/* Modified: 2023-02-23                         */
 /************************************************/
 
 #! /usr/bin/bash
