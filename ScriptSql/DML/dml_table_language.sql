3508c45b4955739cfd5a7efed67832c41277408b
Luca Pelorosso <lpelo2000@gmail.com>
2023-05-27 11:20:47 +0200

    LP 20230527: Aggiunto Cinese

--- ScriptSql/DML/dml_table_language.sql
+++ ScriptSql/DML/dml_table_language.sql
@@ -2,8 +2,8 @@
 /* Created by: Luca Pelorosso                   */
 /* Created: 2023-05-09                          */
 /* Modified by: Luca Pelorosso                  */
-/* Modified: 2023-05-12                         */
-/* Definition: insert data table language        */
+/* Modified: 2023-05-27                         */
+/* Definition: insert data table language       */
 /************************************************/
  
 SET SQL_SAFE_UPDATES = 0;
@@ -22,7 +22,9 @@ VALUES ('German');
 VALUES ('Spanish'); 
 COMMIT;
  
-
+INSERT INTO gestionale.language (language_name)
+VALUES ('Chinese'); 
+COMMIT;
 /************************************************/
 /* Created: 2023/05/09                          */
 /* Created By: Luca Pelorosso                   */
