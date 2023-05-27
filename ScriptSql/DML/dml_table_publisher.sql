4b0e4327f1cfeb9c8681b15f594c925b9045d7a6
Luca Pelorosso <lpelo2000@gmail.com>
2023-05-27 11:09:51 +0200

    prova

--- ScriptSql/DML/dml_table_publisher.sql
+++ ScriptSql/DML/dml_table_publisher.sql
@@ -2,7 +2,7 @@
 /* Created by: Luca Pelorosso                   */
 /* Created: 2022-10-28                          */
 /* Modified by: Luca Pelorosso                  */
-/* Modified: 2023-05-10                         */
+/* Modified: 2023-05-11                         */
 /* Definition: insert data table publisher      */
 /************************************************/
 
