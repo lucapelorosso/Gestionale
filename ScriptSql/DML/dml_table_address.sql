5ac1ddc7cd05369496aaa90d309b38958a8ecd06
Luca Pelorosso <lpelo2000@gmail.com>
2023-06-01 10:59:48 +0200

    LP: Aggiunta libreria e indirizzo

--- ScriptSql/DML/dml_table_address.sql
+++ ScriptSql/DML/dml_table_address.sql
@@ -2,7 +2,7 @@
 /* Created by: Luca Pelorosso                   */
 /* Created: 2022-10-28                          */
 /* Modified by: Luca Pelorosso                  */
-/* Modified: 2023-05-10                         */
+/* Modified: 2023-06-01                         */
 /* Definition: insert data table address        */
 /************************************************/
 
@@ -104,6 +104,24 @@ VALUES
 'Sesto San Giovanni',
 'Lamarmora',
 '9');
+COMMIT;
+
+INSERT INTO gestionale.address
+(
+`nationID`,
+`cap`,
+`tipo`,
+`city`,
+`a_address`,
+`a_number`)
+VALUES
+(
+(select nationID from gestionale.nation where nation_name = 'Italia'),
+'20099',
+'Piazza',
+'Sesto San Giovanni',
+'Martiri di via Fani',
+'1');
 COMMIT;
 
 
--- ScriptSql/DML/dml_table_bookstore_shops.sql
+++ ScriptSql/DML/dml_table_bookstore_shops.sql
@@ -23,7 +23,23 @@ null,
 (SELECT nationID FROM gestionale.nation where nation_name = 'Italia'),
 (SELECT indirizzo.addressID FROM gestionale.address indirizzo WHERE indirizzo.city = 'Sesto San Giovanni' AND a_address = 'Lamarmora' AND indirizzo.a_number = '9')
 );
+COMMIT;
 
+INSERT INTO `gestionale`.`bookstore_shops`
+( 
+`bookstoreName`,
+`foundation_date`,
+`closed_date`,
+`nationID`,
+`addressID`)
+VALUES
+( 
+'Libreria Tarantola',
+STR_TO_DATE('02/01/2000', '%d/%m/%Y'),
+null,
+(SELECT nationID FROM gestionale.nation where nation_name = 'Italia'),
+(SELECT indirizzo.addressID FROM gestionale.address indirizzo WHERE indirizzo.city = 'Sesto San Giovanni' AND a_address = 'Martiri di via Fani' AND indirizzo.a_number = '1')
+);
 COMMIT;
 /************************************************/
 /* Created: 2023-05-16                          */
