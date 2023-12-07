c58596c774699dc15997b4b3b32e0ba26c40e9ba
Luca Pelorosso <lpelo2000@gmail.com>
2023-12-07 21:41:05 +0100

    LP 20231207: create utenze per db in locale sul mac

--- ScriptSql/DML/dml_creazione_user.sql
+++ ScriptSql/DML/dml_creazione_user.sql
@@ -2,12 +2,17 @@
 /* Created by: Luca Pelorosso                   */
 /* Created: 2022-10-18                          */
 /* Modified by: Luca Pelorosso                  */
-/* Modified: 2022-10-19                         */
+/* Modified: 2023-12-07                         */
 /************************************************/
 
 -- Drop User
 DROP USER 'admin'@"ls-ffabf499b0f8332f4e19ea3361e9799960122e1b.cpbsrhnvchf7.eu-west-3.rds.amazonaws.com";
-
+DROP USER 'gestionale'@'localhost' IDENTIFIED BY 'gestionale1234';
+DROP USER 'admin'@'localhost' IDENTIFIED BY 'gestionale1234';
 
 -- Create User
 CREATE USER 'admin'@"ls-ffabf499b0f8332f4e19ea3361e9799960122e1b.cpbsrhnvchf7.eu-west-3.rds.amazonaws.com" IDENTIFIED BY 'gestionale';
+CREATE USER 'gestionale'@'localhost' IDENTIFIED BY 'gestionale1234';
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'gestionale1234';
+
+
