3f85d3428c61b35f848c4dd345f633fba11add5d
Luca Pelorosso <lpelo2000@gmail.com>
2023-12-08 20:29:34 +0100

    LP 20231208 store procedure

---
+++ ScriptSql/DDL/ddl_create_procedure_type_account.sql
@@ -0,0 +1,14 @@
+/************************************************/
+/* Created by: Luca Pelorosso                   */
+/* Created: 2023-12-08                          */
+/* Modified by: Luca Pelorosso                  */
+/* Modified: 2023-12-08                         */
+/************************************************/
+
+
+DELIMITER $$
+CREATE DEFINER=`gestionale`@`localhost` PROCEDURE `procedure_type_account`()
+BEGIN
+
+END$$
+DELIMITER ;
