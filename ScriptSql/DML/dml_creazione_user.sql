3361eab896570af6b12df2e4c08051c0e2b97b04
Luca Pelorosso <lpelo2000@gmail.com>
2024-03-09 18:47:45 +0100

    LP 20240309: utente root

--- ScriptSql/DML/dml_creazione_user.sql
+++ ScriptSql/DML/dml_creazione_user.sql
@@ -9,10 +9,11 @@
 DROP USER 'admin'@"ls-ffabf499b0f8332f4e19ea3361e9799960122e1b.cpbsrhnvchf7.eu-west-3.rds.amazonaws.com";
 DROP USER 'gestionale'@'localhost' IDENTIFIED BY 'gestionale1234';
 DROP USER 'admin'@'localhost' IDENTIFIED BY 'gestionale1234';
+DROP USER 'root'@'localhost' IDENTIFIED BY 'gestionale1234';
 
 -- Create User
 CREATE USER 'admin'@"ls-ffabf499b0f8332f4e19ea3361e9799960122e1b.cpbsrhnvchf7.eu-west-3.rds.amazonaws.com" IDENTIFIED BY 'gestionale';
 CREATE USER 'gestionale'@'localhost' IDENTIFIED BY 'gestionale1234';
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'gestionale1234';
-
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'gestionale1234';
 
